cd9ef6f46e9aa84a4226a5a7f2673ca250ff57e5
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-08-10 18:06:39 +0600

    academic faculty, department

--- src/pages/admin/academicManagement/AcademicDepartment.tsx
+++ src/pages/admin/academicManagement/AcademicDepartment.tsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import { useState } from "react";
+import { TQueryParam } from "../../../types";
+import { useGetAllDepartmentsQuery } from "../../../redux/features/admin/departmentManagement.api";
+import { Button, Table } from "antd";
 
 const AcademicDepartment = () => {
+    // const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+    const { data: academicSemesterData, isFetching } = useGetAllDepartmentsQuery(params);
+
+    const tableData = academicSemesterData?.data?.map(({ _id, name, academicFaculty }) => ({
+        key: _id,
+        name,
+        academicFaculty: academicFaculty.name
+    }))
+
+
+    const columns = [
+        {
+            title: 'Name',
+            key: "name",
+            dataIndex: 'name',
+            // filters: [
+            //     {
+            //         text: 'Autumn',
+            //         value: 'Autumn',
+            //     },
+            //     {
+            //         text: 'Summer',
+            //         value: 'Summer',
+            //     },
+            //     {
+            //         text: 'Fall',
+            //         value: 'Fall',
+            //     },
+            // ],
+        },
+        {
+            title: "Academic Faculty",
+            name: 'academicFaculty',
+            dataIndex: 'academicFaculty',
+        },
+        {
+            title: "Action",
+            key: "X",
+            render: () => {
+                return (
+                    <div>
+                        <Button>Update</Button>
+                    </div>
+                )
+            }
+        }
+    ];
+
+
+    // const onChange: TableProps<TTableData>['onChange'] = (_pagination, filters, _sorter, extra) => {
+    //     if (extra.action === "filter") {
+    //         const queryParams: TQueryParam[] = [];
+
+    //         filters.name?.forEach((item) => (
+    //             queryParams.push({ name: "name", value: item })
+    //         ));
+
+    //         setParams(queryParams);
+    //     }
+    // };
+
     return (
-        <div>AcademicDepartment</div>
+        <Table
+            columns={columns}
+            loading={isFetching}
+            dataSource={tableData}
+            // onChange={onChange}
+            showSorterTooltip={{ target: 'sorter-icon' }}
+        />
     )
 }
 
--- src/redux/features/admin/departmentManagement.api.tsx
+++ src/redux/features/admin/departmentManagement.api.tsx
@@ -4,7 +4,23 @@ import { baseApi } from "../../api/baseApi";
 
 const departmentManagementApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
+        getAllDepartments: builder.query({
+            query: (args) => {
+                const params = new URLSearchParams();
 
+                if (args) {
+                    args.forEach((item) => {
+                        params.append(item.name, item.value as string)
+                    })
+                }
+
+                return {
+                    url: "/academic-departments",
+                    method: "GET",
+                    params: params
+                }
+            }
+        }),
         addDepartment: builder.mutation({
             query: (data) => ({
                 url: "academic-departments/create-academic-department",
@@ -13,9 +29,8 @@ const departmentManagementApi = baseApi.injectEndpoints({
             })
         })
 
-
     })
 });
 
 
-export const { useAddDepartmentMutation } = departmentManagementApi;
+export const { useAddDepartmentMutation, useGetAllDepartmentsQuery } = departmentManagementApi;
